a66da683bf1a930371aae77983eb6bebec0f6354
Jiyoung Park <78076790+kkdd0757@users.noreply.github.com>
2025-02-04 11:19:16 -0500

    Time: 1 ms (79.13%), Space: 51.9 MB (37.38%) - LeetHub

---
+++ 0274-h-index/0274-h-index.ts
@@ -0,0 +1,8 @@
+function hIndex(citations: number[]): number {
+    citations.sort((a,b) => b - a);
+
+    for(let i = 0 ; i < citations.length; i++) {
+        if(citations[i] < i + 1) return i;
+    }
+    return citations.length;
+};

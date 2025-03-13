ff77a8de5a16ee4ee45abb90db5567be4e1f13d2
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-03-13 19:11:35 -0400

    Time: 0 ms (100%), Space: 55.4 MB (24.66%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.ts
+++ 0228-summary-ranges/0228-summary-ranges.ts
@@ -1,26 +1,18 @@
-const makeRange = (start: number, end: number):string => {
-    return `${start}->${end}`;
+const convertAnswer = (a: number, b: number) => {
+    return `${a}->${b}`;
 }
-
 function summaryRanges(nums: number[]): string[] {
     if(nums.length <= 0) return [];
-
-    const answer: string[] = [];
+    const answer = [];
     let start = 0;
-
     for(let i = 1; i < nums.length; i++) {
-        if(nums[i] > nums[i - 1] + 1) {
-            if(i-1 === start) answer.push(`${nums[start]}`);
-            else {
-                const range = makeRange(nums[start], nums[i-1]);
-                 answer.push(range);
-            }
+        if(nums[i] > nums[i-1] + 1) {
+            if(nums[i-1] === nums[start]) answer.push(`${nums[start]}`);
+            else answer.push(convertAnswer(nums[start], nums[i-1]));
             start = i;
         }
     }
-    if(start !== nums.length - 1) {
-        const range = makeRange(nums[start], nums[nums.length - 1]);
-        answer.push(range);
-    } else answer.push(`${nums[start]}`);
+    if(start !== nums.length - 1) answer.push(convertAnswer(nums[start], nums[nums.length-1]));
+    else answer.push(`${nums[nums.length - 1]}`);
     return answer;
 };

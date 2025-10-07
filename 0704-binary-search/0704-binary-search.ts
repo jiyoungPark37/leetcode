5a21832e706913a4a83ec159fe32cb4e5e2fe518
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-10-07 17:49:07 -0400

    Time: 0 ms (100%), Space: 58.5 MB (70.3%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.ts
@@ -0,0 +1,12 @@
+function search(nums: number[], target: number): number {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while(start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        if(nums[mid] === target) return mid;
+        if(nums[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    return -1;
+};

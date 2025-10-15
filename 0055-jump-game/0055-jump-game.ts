69cf5774a39479ed98a0105d667d42d8d67c60d5
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-10-15 18:52:03 -0400

    Time: 2 ms (67.47%), Space: 60 MB (39.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.ts
+++ 0055-jump-game/0055-jump-game.ts
@@ -1,26 +1,8 @@
-// solution1 : using array
-// function canJump(nums: number[]): boolean {
-//     const array = Array(nums.length).fill(false);
-//     array[0] = true;
-
-//     for(let i = 0; i < nums.length; i++) {
-//         if(array[i]) {
-//             for(let j = 1; j <= nums[i]; j++) {
-//                 if (i + j < nums.length) {  
-//                     array[i+j] = true;
-//                 }
-//             }
-//         } else return false;
-//     }
-//     return array[array.length - 1];
-// };
-// solution2
 function canJump(nums: number[]): boolean {
-    let currentMoveAcc = nums[0];
-
-    for(let i = 1; i < nums.length; i++) {
-        if(currentMoveAcc === 0) return false;
-        currentMoveAcc = Math.max(currentMoveAcc - 1, nums[i]);
+    let curMax = 0;
+    for(let i = 0; i < nums.length; i++) {
+        curMax = Math.max(curMax - 1, nums[i]);
+        if(curMax <= 0 && i < nums.length - 1) return false;
     }
     return true;
 };

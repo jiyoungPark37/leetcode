f88df7b1f2c54ced77832a4ab1de082a28cf5084
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-10-17 09:29:10 -0400

    Time: 2 ms (63.89%), Space: 52 MB (100%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,16 +1,13 @@
 function jump(nums: number[]): number {
-    let left = 0;
-    let right = 0;
-    let answer = 0;
-
-    while(right < nums.length - 1) {
-        let farthest = 0;
-        for(let i = left; i <= right; i++) {
-            farthest = Math.max(farthest, nums[i]+i);
-        };
-        left = right + 1;
-        right = farthest;
-        answer++;
-    };
-    return answer;
+    let maxDiff = 0;
+    let count = 0;
+    let curPos = 0;
+    for(let i = 0; i < nums.length - 1; i++) {
+        maxDiff = Math.max(maxDiff, i + nums[i]);
+        if(i === curPos) {
+            curPos = maxDiff;
+            count++;
+        }
+    }
+    return count;
 };

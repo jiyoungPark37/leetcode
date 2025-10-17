e39669df4a0ed3238b11d9c9d41218e3c3eafcaa
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-10-17 10:16:49 -0400

    Time: 36 ms (67.86%), Space: 62.6 MB (89.29%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.ts
+++ 0846-hand-of-straights/0846-hand-of-straights.ts
@@ -1,24 +1,22 @@
 function isNStraightHand(hand: number[], groupSize: number): boolean {
-    if(hand.length % groupSize !== 0) return false;
+  if (hand.length % groupSize !== 0) return false;
 
-    const map = new Map();
-    hand.forEach((ele) => {
-        map.set(ele, (map.get(ele) ?? 0) + 1);
-    });
+  hand.sort((a, b) => a - b);
 
-    hand.sort((a,b) => a - b);
-    for(let i = 0; i < hand.length; i++) {
-        if(map.get(hand[i]) > 0) {
-            const first = hand[i];
-            map.set(first, map.get(first) - 1);
-            if(map.get(first) === 0) map.delete(first);
-            for(let j = 1; j < groupSize; j++) {
-                if(!map.has(first + j)) return false;
-                map.set(first + j, map.get(first + j) - 1);
-                if(map.get(first + j) === 0) map.delete(first + j);
-            }
-        }
+  const map = new Map<number, number>();
+  hand.forEach(ele => map.set(ele, (map.get(ele) ?? 0) + 1));
+
+  for (let i = 0; i < hand.length; i++) {
+    const curVal = hand[i];
+    const cur = map.get(curVal);
+    if (cur > 0) {
+      for (let j = curVal; j < curVal + groupSize; j++) {
+        const count = map.get(j);
+        if (!count || count <= 0) return false;
+        map.set(j, count - 1);
+      }
     }
-    if(map.size > 0) return false;
-    return true;
-};
+  }
+
+  return true;
+}

66b4d6b3d81f55a661e67b19aef20d8245ba0c1e
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-10-20 13:59:25 -0400

    Time: 0 ms (100%), Space: 55.5 MB (64.61%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(root === null) return root;
+    let prevLeft = root.left;
+    let prevRight = root.right;
+    root.left = invertTree(prevRight);
+    root.right = invertTree(prevLeft);
+    return root;
+};

1ba0b1c8d5a4117cdd4c2f0679c26286d396260c
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-11-13 12:38:27 -0500

    Time: 4 ms (55.1%), Space: 57.7 MB (27.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -2,8 +2,12 @@ function twoSum(nums: number[], target: number): number[] {
     const map = new Map();
 
     for(let i = 0; i < nums.length; i++) {
-        const total = target - nums[i];
-        if(map.has(total)) return [map.get(total), i];
-        map.set(nums[i], i);
+        const num = nums[i];
+        const minusNum = target - num;
+        if(map.has(minusNum)) {
+            return [map.get(minusNum), i];
+        } else {
+            map.set(num, i);
+        }
     }
 };

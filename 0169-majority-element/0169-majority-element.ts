2d4eb1573718171aa137f686b898a9e5f66ab0bb
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-11-14 13:14:55 -0500

    Time: 10 ms (11.69%), Space: 60.7 MB (11.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,5 +1,17 @@
 function majorityElement(nums: number[]): number {
     nums.sort((a,b) => a - b);
-    const mid = Math.floor(nums.length / 2);
-    return nums[mid];
+    const max = Math.ceil(nums.length / 2);
+
+    let count = 0;
+    let prev = Infinity;
+    for(const n of nums) {
+        if(prev !== n) {
+            prev = n;
+            count = 1;
+        } else {
+            count+=1;
+            if(count >= max) return n;
+        }
+    }
+    return prev;
 };

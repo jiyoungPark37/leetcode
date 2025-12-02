8de5a5dc18fc95e89e775d4a99fe125d4a61c1e3
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-02 11:15:48 -0500

    Time: 60 ms (11.68%), Space: 84.9 MB (6.09%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -1,15 +1,50 @@
+class MinStack {
+    private stack: number[][];
+    private minStack: number[][];
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val: number[]) {
+        this.stack.push(val);
+
+        const currentMin = this.minStack.length >  0 ? this.minStack[this.minStack.length - 1][0] : Infinity;
+        if(currentMin >= val[0]) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        if(this.stack.length <= 0) return;
+        const popped = this.stack.pop();
+
+        if(this.minStack.length > 0 && this.minStack[this.minStack.length - 1][0] === popped[0]) {
+            this.minStack.pop();
+        }
+        return popped;
+    }
+    getMin() {
+        if(this.minStack.length === 0) return;
+        return this.minStack[this.minStack.length - 1][0];
+    }
+    top() {
+        if(this.stack.length === 0) return;
+        return this.stack[this.stack.length - 1][0];
+    }
+}
 function dailyTemperatures(temperatures: number[]): number[] {
-    const stack = [];
-    const answer = Array(temperatures.length).fill(0);
+    const minStack = new MinStack();
+    const arr = Array(temperatures.length).fill(0);
 
     for(let i = 0; i < temperatures.length; i++) {
-        while(stack.length > 0 && stack[stack.length - 1][0] < temperatures[i]) {
-            const popped = stack.pop();
-            const idx = popped[1];
-
-            answer[idx] = i - idx;
-        }
-        stack.push([temperatures[i], i]);
+        const cur = temperatures[i];
+        while (minStack.getMin() && minStack.getMin() < cur) {
+            const popped = minStack.pop();
+            const diff = i - popped[1];
+            arr[popped[1]] = diff;
+        } 
+        minStack.push([cur, i]);
     }
-    return answer;
+    return arr;
 };

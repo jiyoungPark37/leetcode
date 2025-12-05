052e8068bfac81e887ec002dc5ab8c78cb341e22
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-05 12:38:04 -0500

    Time: 2 ms (85.37%), Space: 60.6 MB (91.22%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function averageOfLevels(root: TreeNode | null): number[] {
+    const answer = [];
+    const queue = [root];
+
+    while(queue.length > 0) {
+        const len = queue.length;
+        let sum = 0;
+        for(let i = 0; i < len; i++) {
+            const first = queue.shift();
+            if(first?.val != undefined) sum += first.val;
+            if(first?.left) queue.push(first.left);
+            if(first?.right) queue.push(first.right);
+        }
+        answer.push(sum / len);
+    }
+    return answer;
+};

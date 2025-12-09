bb4acdcd4aa18d13de532b3f2748da24fcad3223
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-08 23:38:34 -0500

    Time: 3 ms (84.81%), Space: 60.5 MB (32.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,4 +1,4 @@
 function isPalindrome(s: string): boolean {
-    const str = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+    const str = s.trim().toLowerCase().replace(/[^0-9a-z]+/g, '');
     return str === str.split('').reverse().join('');
 };

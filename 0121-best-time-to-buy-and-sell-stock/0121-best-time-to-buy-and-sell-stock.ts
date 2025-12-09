ebf830bf107b224216e11cec039b18158c716368
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-08 22:37:14 -0500

    Time: 3 ms (54.39%), Space: 65.8 MB (65.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,10 +1,11 @@
 function maxProfit(prices: number[]): number {
-    let maxDiff = 0;
     let minPrice = Infinity;
+    let maxCost = -Infinity;
 
     for(let i = 0; i < prices.length; i++) {
-        maxDiff = Math.max(maxDiff, prices[i] - minPrice);
-        minPrice = Math.min(minPrice, prices[i]);
+        const todayPrice = prices[i];
+        minPrice = Math.min(minPrice, todayPrice);
+        maxCost = Math.max(todayPrice - minPrice, maxCost);
     }
-    return maxDiff;
+    return maxCost;
 };

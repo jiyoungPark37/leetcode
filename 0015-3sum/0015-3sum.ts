271c6ee839c53bc297ad4642e2e38063b16ae4da
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 10:28:59 -0500

    Time: 20 ms (99.71%), Space: 73.2 MB (63.06%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,20 +1,26 @@
 function threeSum(nums: number[]): number[][] {
     const answer = [];
-    const set = new Set();
     nums.sort((a,b) => a - b);
 
     for(let i = 0; i < nums.length - 2; i++) {
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+        // if the smallest value is bigger than 0, there is no chance that the sum is 0
+        if(nums[i] > 0) break;
+
         let start = i + 1;
         let end = nums.length - 1;
-        while(start < end) {
-            const key = `${nums[i]}-${nums[start]}-${nums[end]}`;
-            const sum = nums[i] + nums[start] + nums[end];
-            if(sum === 0 && !set.has(key)) {
-                set.add(key);
-                answer.push([nums[i], nums[start], nums[end]])
+        while(start<end) {
+            const sum = nums[start] + nums[end] + nums[i];
+            if(sum === 0) {
+                 answer.push([nums[i], nums[start], nums[end]]);
+                 while(nums[start] === nums[start+1] && start < end) start++;
+                 while(start<end && nums[end] === nums[end - 1]) end--;
+                start++;
+                end--;
+            } else {
+                if(sum > 0) end-=1;
+                else start+=1;
             }
-            if(sum >= 0) end-=1;
-            else start+=1;
         }
     }
     return answer;

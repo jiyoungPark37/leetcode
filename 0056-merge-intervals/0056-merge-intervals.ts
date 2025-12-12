1247fb398b5f5d59f3f10177e21cf3b302214df6
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 10:43:41 -0500

    Time: 18 ms (16.43%), Space: 67.9 MB (13.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -1,19 +1,18 @@
 function merge(intervals: number[][]): number[][] {
-    intervals.sort((a,b) => a[0] - b[0]);
     const answer = [];
+    intervals.sort((a,b) => a[0] - b[0]);
 
-    let start = intervals[0][0];
-    let end = intervals[0][1];
-    for(let i = 1; i < intervals.length; i++) {
-        if(start <= intervals[i][0] && intervals[i][0] <= end) {
-            start = Math.min(intervals[i][0], start);
-            end = Math.max(end, intervals[i][1]);
+    let prev = intervals[0][0];
+    let next = intervals[0][1];
+    for(const [start, end] of intervals) {
+        if(start <= next) {
+            next = Math.max(next, end);
         } else {
-            answer.push([start, end]);
-            start = intervals[i][0];
-            end = intervals[i][1];
+            answer.push([prev, next]);
+            prev = start;
+            next = end;
         }
     }
-    answer.push([start, end]);
+    answer.push([prev, next]);
     return answer;
 };

ee2300d4061d0763bf439b9e98bcfd549d30c834
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 09:34:37 -0500

    Time: 41 ms (27.1%), Space: 70.5 MB (14.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,10 +1,9 @@
 function groupAnagrams(strs: string[]): string[][] {
     const map = new Map();
 
-    for(let i = 0; i < strs.length; i++) {
-        const sorted = strs[i].split('').sort().join('');
-        map.set(sorted, [...(map.get(sorted) ?? []), strs[i]]);
-    } 
-    const arr = Array.from(map.values());
-    return arr;
+    strs.forEach(str => {
+        const sortedStr = str.split('').sort((a,b) => a.localeCompare(b)).join('');
+        map.set(sortedStr, [...(map.get(sortedStr) ?? []), str]);
+    });
+    return Array.from(map.values());
 };

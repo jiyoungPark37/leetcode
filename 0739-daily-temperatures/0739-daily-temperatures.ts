33fbfc574baabb20c5f1e2b620b7314dcad1c3c6
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 13:30:53 -0500

    Time: 44 ms (23.74%), Space: 82.8 MB (12.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -1,50 +1,15 @@
-class MinStack {
-    private stack: number[][];
-    private minStack: number[][];
-    constructor() {
-        this.stack = [];
-        this.minStack = [];
-    }
-
-    push(val: number[]) {
-        this.stack.push(val);
-
-        const currentMin = this.minStack.length >  0 ? this.minStack[this.minStack.length - 1][0] : Infinity;
-        if(currentMin >= val[0]) {
-            this.minStack.push(val);
-        }
-    }
-
-    pop() {
-        if(this.stack.length <= 0) return;
-        const popped = this.stack.pop();
-
-        if(this.minStack.length > 0 && this.minStack[this.minStack.length - 1][0] === popped[0]) {
-            this.minStack.pop();
-        }
-        return popped;
-    }
-    getMin() {
-        if(this.minStack.length === 0) return;
-        return this.minStack[this.minStack.length - 1][0];
-    }
-    top() {
-        if(this.stack.length === 0) return;
-        return this.stack[this.stack.length - 1][0];
-    }
-}
 function dailyTemperatures(temperatures: number[]): number[] {
-    const minStack = new MinStack();
     const arr = Array(temperatures.length).fill(0);
 
+    const stack = [];
     for(let i = 0; i < temperatures.length; i++) {
         const cur = temperatures[i];
-        while (minStack.getMin() && minStack.getMin() < cur) {
-            const popped = minStack.pop();
-            const diff = i - popped[1];
-            arr[popped[1]] = diff;
-        } 
-        minStack.push([cur, i]);
+        while(stack.length > 0 && stack[stack.length - 1][0] < cur) {
+            const popped = stack.pop();
+            const prevIdx = popped[1];
+            arr[prevIdx] = i - prevIdx;
+        }
+        stack.push([cur, i]);
     }
     return arr;
 };

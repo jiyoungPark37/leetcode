d4ba1511ae7a013114f8b39997a1609ea3ccc21f
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 13:11:34 -0500

    Time: 0 ms (100%), Space: 58.8 MB (26.63%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -13,16 +13,15 @@
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     if(!list1 && !list2) return null;
 
-    let answer = new ListNode(0);
+    const answer = new ListNode(0);
     let temp = answer;
-
     while(list1 && list2) {
-        if(list1.val <= list2.val) {
-            temp.next = list1;
-            list1 = list1.next;
-        } else {
+        if(list1.val >= list2.val) {
             temp.next = list2;
             list2 = list2.next;
+        } else {
+            temp.next = list1;
+            list1 = list1.next;
         }
         temp = temp.next;
     }

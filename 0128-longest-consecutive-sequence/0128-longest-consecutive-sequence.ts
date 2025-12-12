15161e18fbe6fd15888215191dd684b868cf337f
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-12 09:47:41 -0500

    Time: 30 ms (98.33%), Space: 68.3 MB (90.98%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -1,19 +1,15 @@
 function longestConsecutive(nums: number[]): number {
-    if(nums.length <= 1) return nums.length;
     nums.sort((a,b) => a - b);
+    let answer = 0;
     let count = 1;
-    let answer = 1;
-    let prev = nums[0];
-    for(let i = 1; i < nums.length; i++) {
-        if(prev === nums[i]) continue;
-        if(prev + 1 === nums[i]) {
-            count++;
-        } else {
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === nums[i+1]) continue;
+        if(nums[i]+1 === nums[i+1]) count+=1;
+        else {
             if(answer < count) answer = count;
             count = 1;
         }
-        prev = nums[i];
     }
-    answer = Math.max(answer, count);
     return answer;
 };

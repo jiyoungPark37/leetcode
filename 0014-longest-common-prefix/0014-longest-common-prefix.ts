52ce6127ad188c5dabc8e97f9ad6d97c793cf1bb
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 17:03:47 -0500

    Time: 5 ms (22.49%), Space: 56.4 MB (43.8%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -1,17 +1,12 @@
 function longestCommonPrefix(strs: string[]): string {
-    if(strs.length <= 0) return '';
-
     strs.sort((a,b) => a.length - b.length);
-    let std = strs[0];
-    for(let i = 1; i < strs.length; i++) {
-        const compareStr = strs[i];
-        let idx = 0;
-        for(let j = 0; j < compareStr.length; j++) {
-            if(compareStr[j] !== std[idx]) {
-                std = std.slice(0, idx);
-                break;
-            } else idx++;
-        }
+    const std = strs[0];
+    let cur = '';
+    for(let i = 1; i <= std.length; i++){
+        const substr = std.substring(0,i);
+        const check = strs.every(str => str.startsWith(substr));
+        if(!check) return cur;
+        cur = substr;
     }
-    return std;
+    return cur;
 };

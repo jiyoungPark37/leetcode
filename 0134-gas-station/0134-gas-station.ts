6fc09ee6214de9bd582b78dc16adc6083f50c7ce
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 16:05:24 -0500

    Time: 1 ms (89%), Space: 66.1 MB (84.75%) - LeetHub

--- 0134-gas-station/0134-gas-station.ts
+++ 0134-gas-station/0134-gas-station.ts
@@ -1,15 +1,16 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    let res = 0;
-    let distance = 0;
+    let start = 0;
+    let totalDiff = 0;
+    let tank = 0;
 
-    if(gas.reduce((a,b) => a + b, 0) - cost.reduce((a,b) => a + b, 0) < 0) return -1;
-    
-    for(let i = 0; i < gas.length; i++){
-        distance += gas[i] - cost[i];
-        if(distance < 0) {
-            distance = 0;
-            res = i + 1;
+    for(let i = 0; i < gas.length; i++) {
+        const diff = gas[i] - cost[i];
+        tank += diff;
+        totalDiff += diff;
+        if(tank < 0) {
+            start = i + 1;
+            tank = 0;
         }
-    };
-    return res;
+    }
+    return totalDiff < 0 ? -1 : start;
 };

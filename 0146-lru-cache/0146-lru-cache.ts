b15444a816333b2157bacea4b7b7ab660125ff47
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 18:53:45 -0500

    Time: 106 ms (45.1%), Space: 107.4 MB (89.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.ts
+++ 0146-lru-cache/0146-lru-cache.ts
@@ -1,30 +1,26 @@
 class LRUCache {
-    private map: Map<number,number> = new Map();
     private capacity: number;
+    private map: Map<number, number>;
     constructor(capacity: number) {
+        this.map = new Map();
         this.capacity = capacity;
     }
 
     get(key: number): number {
-        const map = this.map;
-        if(!map.has(key)) return -1;
-        const ans = map.get(key);
-        this.put(key, ans);
-        return ans;
+        const val = this.map.get(key);
+        if(val == undefined) return -1;
+
+        this.map.delete(key);
+        this.map.set(key, val);
+        return val;
     }
 
     put(key: number, value: number): void {
-        const map = this.map;
-        if(map.has(key)) {
-            map.delete(key);
-            map.set(key, value);
-        } else {
-            if(map.size >= this.capacity) {
-                const firstKey = Array.from(map.keys())[0];
-                map.delete(firstKey);
-            }
-            map.set(key, value);
+        if(this.map.size>=this.capacity && !this.map.has(key)) {
+            this.map.delete(this.map.keys().next().value);
         }
+        this.map.delete(key);
+        this.map.set(key, value);
     }
 }
 

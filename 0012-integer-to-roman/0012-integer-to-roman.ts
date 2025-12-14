d6610a39f81a388d1d1150a1ef580733bc8b3785
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 16:57:32 -0500

    Time: 2 ms (97.84%), Space: 62 MB (63.45%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.ts
+++ 0012-integer-to-roman/0012-integer-to-roman.ts
@@ -1,41 +1,15 @@
-const INTEGER_TO_ROMAN = {
-    1: {
-        1: 'I',
-        5: 'V',
-        10: 'X',
-    }, 
-    2: {
-        1: 'X',
-        5: 'L',
-        10: 'C'
-    },
-    3: {
-        1: 'C',
-        5: 'D',
-        10: 'M'
-    },
-    4: {
-        1: 'M',
-    }
-}
-const convertNumToRoman = (num: number, digit: number): string => {
-    const obj = INTEGER_TO_ROMAN[digit];
-    if(num === 0) return '';
-    if(num >= 1 && num <= 3) return `${obj[1].repeat(num)}`
-    if(num === 4) return `${obj[1]}${obj[5]}`;
-    if(num === 5) return obj[num];
-    if(num < 9) return `${obj[5]}${obj[1].repeat(num - 5)}`;
-    return `${obj[1]}${obj[10]}`;
-}
 function intToRoman(num: number): string {
-    const numToString = String(num);
-    const len = numToString.length;
-    const answer: string[] = Array(len).fill('');
-
-    for(let i = 0; i < len; i++) {
-        const digit = len - i;
-        const currentNum = convertNumToRoman(Number(numToString[i]), digit);
-        answer[i] = currentNum;
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    
+    let result = "";
+    
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
+        }
     }
-    return answer.join('');
+    
+    return result;
 };

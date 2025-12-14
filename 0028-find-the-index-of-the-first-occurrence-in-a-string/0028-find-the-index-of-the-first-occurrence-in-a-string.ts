a9a0ce473059e788686136eeea014e2c05dcf262
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 17:40:39 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,16 +1,12 @@
 function strStr(haystack: string, needle: string): number {
     if(needle.length > haystack.length) return -1;
+    if(!haystack.includes(needle)) return -1;
 
-    let left = 0;
-    for(let i = 0; i < haystack.length; i++) {
-        let idx = i;
-        while(haystack[idx] === needle[left]) {
-            console.log(idx, left, 'left and idx', haystack[idx], needle[left])
-            idx++;
-            left++;
-            if(left === needle.length) return i;
-        } 
-        left = 0;
+    const len = needle.length;
+    for(let i = 0; i < haystack.length - len + 1; i++) {
+        if(haystack[i] !== needle[0]) continue;
+        const substr = haystack.slice(i, len);
+        if(substr === needle) return i;
     }
     return -1;
 };

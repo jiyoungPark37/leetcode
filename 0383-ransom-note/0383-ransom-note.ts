9af797dd88a83d6c3c970478f7841d046dbb8d30
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-14 17:47:55 -0500

    Time: 14 ms (75.6%), Space: 59.6 MB (59.9%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.ts
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -1,12 +1,12 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    if(magazine.length < ransomNote.length) return false;
-
+    if(ransomNote.length > magazine.length) return false;
     const map = new Map();
-    magazine.split('').forEach(ele => map.set(ele, (map.get(ele) ?? 0) + 1));
+    for(const ch of magazine){
+        map.set(ch, (map.get(ch) ?? 0) + 1);
+    }
     for(const ch of ransomNote) {
-        if(map.get(ch) > 0) {
-            map.set(ch, map.get(ch) - 1);
-        } else return false;
-    }  
+        if(!map.has(ch) || map.get(ch) <= 0) return false;
+        map.set(ch, map.get(ch) - 1)
+    }
     return true;
 };

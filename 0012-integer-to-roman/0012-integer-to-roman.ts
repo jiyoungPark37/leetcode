0805bbe32852771682f4b672c7dfa11a584f8d61
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-15 08:06:29 -0500

    Time: 3 ms (89.84%), Space: 61.5 MB (81.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.ts
+++ 0012-integer-to-roman/0012-integer-to-roman.ts
@@ -1,15 +1,13 @@
 function intToRoman(num: number): string {
-    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    
-    let result = "";
-    
-    for (let i = 0; i < values.length; i++) {
-        while (num >= values[i]) {
-            result += symbols[i];
-            num -= values[i];
+    const values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    const romans = ['M','CM', 'D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+
+    let answer = '';
+    for(let i = 0; i < values.length; i++) {
+        while(num >= values[i]) {
+            num-=values[i];
+            answer+=romans[i];
         }
     }
-    
-    return result;
+    return answer;
 };

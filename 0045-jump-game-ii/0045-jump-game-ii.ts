661cf6f92e52528bc8749fef7ec094865a8f1cd7
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-15 10:08:08 -0500

    Time: 2 ms (65.31%), Space: 55.8 MB (93.51%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -2,12 +2,12 @@ function jump(nums: number[]): number {
     let max = 0;
     let count = 0;
     let end = 0;
-
+    
     for(let i = 0; i < nums.length - 1; i++) {
-        max = Math.max(max, i + nums[i]);
-        if(end === i) {
-            end = max;
+        max = Math.max(i+nums[i], max);
+        if(i === end) {
             count++;
+            end = max;
         }
     }
     return count;

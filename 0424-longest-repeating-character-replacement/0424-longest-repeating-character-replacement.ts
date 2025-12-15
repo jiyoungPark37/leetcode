a27b280275cfab04d6ca25efaeffb06e066e9249
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-15 09:04:03 -0500

    Time: 14 ms (80.79%), Space: 57.3 MB (58.13%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
@@ -1,16 +1,18 @@
 function characterReplacement(s: string, k: number): number {
+    let idx = 0;
     const map = new Map();
     let max = 0;
-    let left = 0;
     let answer = 0;
+
     for(let i = 0; i < s.length; i++) {
-        map.set(s[i], (map.get(s[i]) ?? 0) + 1);
-        if(max < map.get(s[i])) max = map.get(s[i]);
-        while(left < i && i - left + 1 > max + k) {
-            map.set(s[left], map.get(s[left]) - 1);
-            left++;
+        const cur = map.get(s[i]) ?? 0;
+        map.set(s[i], cur + 1);
+        max = Math.max(max, cur + 1);
+        while(idx < i && i - idx + 1 > max + k) {
+            map.set(s[idx], map.get(s[idx]) - 1);
+            idx++;
         }
-        answer = Math.max(answer, i - left + 1);
+        answer = Math.max(answer, i - idx + 1);
     }
     return answer;
 };

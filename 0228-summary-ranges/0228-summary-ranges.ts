dc6cb19acea0ab215291fb71f02d0837dcc23598
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-18 09:50:48 -0500

    Time: 0 ms (100%), Space: 54.9 MB (59.58%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.ts
+++ 0228-summary-ranges/0228-summary-ranges.ts
@@ -1,18 +1,19 @@
-const convertAnswer = (a: number, b: number) => {
+const convertToString = (a:number, b?:number) => {
+    if(a === b || b === undefined) return `${a}`;
     return `${a}->${b}`;
-}
+} 
 function summaryRanges(nums: number[]): string[] {
-    if(nums.length <= 0) return [];
     const answer = [];
-    let start = 0;
-    for(let i = 1; i < nums.length; i++) {
-        if(nums[i] > nums[i-1] + 1) {
-            if(nums[i-1] === nums[start]) answer.push(`${nums[start]}`);
-            else answer.push(convertAnswer(nums[start], nums[i-1]));
-            start = i;
+
+    let start = nums[0];
+    for(let i = 0; i < nums.length; i++) {
+        const cur = nums[i];
+        const next = nums[i+1];
+        if(cur+1 === next || cur === next) continue;
+        else {
+            answer.push(convertToString(start, cur));
+            start = next;
         }
     }
-    if(start !== nums.length - 1) answer.push(convertAnswer(nums[start], nums[nums.length-1]));
-    else answer.push(`${nums[nums.length - 1]}`);
     return answer;
 };

abfd25df81d36e133201a0f32a7a37a3f81b4218
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-18 09:58:15 -0500

    Time: 5 ms (42.73%), Space: 61.1 MB (15.24%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.ts
+++ 0057-insert-interval/0057-insert-interval.ts
@@ -2,15 +2,15 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     const newIntervals = [...intervals, newInterval].sort((a,b) => a[0] - b[0]);
 
     const answer = [];
-    let start = newIntervals[0][0];
-    let end = newIntervals[0][1];
-    for(let i = 1; i < newIntervals.length; i++) {
-        if(start <= newIntervals[i][0] && end >= newIntervals[i][0]) {
-            if(end < newIntervals[i][1]) end = newIntervals[i][1];
-        } else {
-            answer.push([start,end]);
-            start = newIntervals[i][0];
-            end = newIntervals[i][1];
+    let start = newIntervals[0][0]
+    let end = newIntervals[0][1]
+    for(const [a,b] of newIntervals) {
+        if(end >= a) {
+            end = Math.max(end, b);
+        }  else {
+            answer.push([start, end]);
+            start = a;
+            end = b;
         }
     }
     answer.push([start, end]);

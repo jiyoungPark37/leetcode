36bce484e0c78a8224e068fe70c6cdaf3674d6c8
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-19 11:06:28 -0500

    Time: 1 ms (87.59%), Space: 60.5 MB (62.48%) - LeetHub

--- 0046-permutations/0046-permutations.ts
+++ 0046-permutations/0046-permutations.ts
@@ -1,20 +1,21 @@
 function permute(nums: number[]): number[][] {
     const answer = [];
-    const set = new Set();
+    const check = Array(nums.length).fill(0);
 
     const dfs = (arr: number[]) => {
         if(arr.length === nums.length) {
-            answer.push([...arr]);
+            answer.push(arr.slice());
             return;
         }
+
         for(let i = 0; i < nums.length; i++) {
-            if(!set.has(i)) {
-                set.add(i);
-                arr.push(nums[i]);
-                dfs(arr);
-                arr.pop(); 
-                set.delete(i);
-            }
+            if(check[i] === 1) continue;
+
+            check[i] = 1;
+            arr.push(nums[i]);
+            dfs(arr);
+            arr.pop();
+            check[i] = 0;
         }
     }
     dfs([]);

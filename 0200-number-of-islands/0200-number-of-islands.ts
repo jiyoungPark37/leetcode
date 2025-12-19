fbba2d74f10ff66c532a6f4cc540861fbd3d11fe
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-18 19:57:40 -0500

    Time: 47 ms (97.64%), Space: 60 MB (60.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -1,27 +1,26 @@
 function numIslands(grid: string[][]): number {
-    let answer = 0;
-    const gridLen = grid.length;
-    const gridRLen = grid[0].length;
     const dir = [[0,1],[1,0],[-1,0],[0,-1]];
+    const n = grid.length;
+    const m = grid[0].length;
 
-    const dfs = (i:number, j:number) => {
-        grid[i][j] = '0';
-        
-        for(let x = 0; x < 4; x++){
-            const nI = i + dir[x][0]
-            const nY = j + dir[x][1];
-            if(nI < gridLen && nY < gridRLen && nI >= 0 && nY >= 0 && grid[nI][nY] === '1') {
-                dfs(nI, nY);
+    const dfs = (x,y) => {
+        grid[x][y] = '0';
+        for(let i = 0; i < 4; i++) {
+            const nx = x + dir[i][0];
+            const ny = y + dir[i][1];
+            if(nx >=0 && ny>=0 && nx<n && ny < m && grid[nx][ny] === '1'){
+                dfs(nx, ny);
             }
         }
     }
-    for(let i = 0; i < gridLen;i++) {
-        for(let j = 0; j < gridRLen; j++) {
+    let count = 0;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
             if(grid[i][j] === '1') {
                 dfs(i, j);
-                answer+=1;
+                count+=1;
             }
         }
     }
-    return answer;
+    return count;
 };

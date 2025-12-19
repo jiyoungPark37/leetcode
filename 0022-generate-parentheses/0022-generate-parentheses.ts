659358acea829d0d9b2dfca1e186f1d9c32aa30a
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-19 12:41:48 -0500

    Time: 2 ms (41.02%), Space: 57.7 MB (19.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.ts
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -1,31 +1,17 @@
-const isValid = (str: string[]):boolean => {
-    let count = 0;
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] === '(') {
-            count+=1;
-        } else {
-            if(count <= 0) return false;
-            count-=1;
-        }
-    }
-    if(count > 0) return false;
-    return true;
-}
 function generateParenthesis(n: number): string[] {
     const len = n * 2;
     const answer = [];
 
     const traverse = (cur: string[], left: number, right: number) => {
         if(cur.length === len) {
-            const res = isValid(cur);
-            if(res) answer.push(cur.join(''));
+            answer.push(cur.join(''));
             return;
         }
 
         if(left < n) {
             traverse([...cur, '('], left + 1, right);
         }
-        if(right < n) {
+        if(right < left) {
             traverse([...cur, ')'], left, right + 1);
         }
     }

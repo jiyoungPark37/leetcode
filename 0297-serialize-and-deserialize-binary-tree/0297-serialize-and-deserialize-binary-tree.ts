eed95bd5b1d31dfcd61ed3a8162f2a0388d7eab7
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-20 08:16:44 -0500

    Time: 82 ms (79.6%), Space: 68.9 MB (63.68%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.ts
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/*
+ * Encodes a tree to a single string.
+ */
+function serialize(root: TreeNode | null): string {
+    const res = [];
+
+    const dfs = (node:TreeNode | null) => {
+        if(node === null) {
+            res.push('#');
+            return;
+        }
+        res.push(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+    dfs(root);
+    return res.join(',');
+};
+
+/*
+ * Decodes your encoded data to tree.
+ */
+function deserialize(data: string): TreeNode | null {
+    const values = data.split(',');
+    let i = 0;
+    const dfs = () => {
+        if(values[i] === '#') {
+            i++;
+            return null;
+        }
+        const node = new TreeNode(Number(values[i]));
+        i++;
+        node.left = dfs();
+        node.right = dfs();
+        return node;
+    }
+    return dfs();
+};
+
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */

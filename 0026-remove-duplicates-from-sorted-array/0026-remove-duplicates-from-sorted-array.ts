363d468630d23156ad653702c0e1e15bb74b19bf
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-20 17:22:59 -0500

    Time: 2 ms (33.13%), Space: 58.6 MB (69.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,12 +1,12 @@
 function removeDuplicates(nums: number[]): number {
-    // 0,0,1,1,1,2,2,3,3,4
-    // 0,1,0,1,1,2,2,3,3,4
+    // [0,1,0,1,1,2,2,3,3,4]
+    // [0,1,1,1,1,2,2,3,3,4]
     let prevIdx = 1;
-
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] === nums[i+1]) continue;
-        nums[prevIdx] = nums[i+1];
-        prevIdx+=1;
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] !== nums[prevIdx-1]) {
+            [nums[i], nums[prevIdx]] = [nums[prevIdx], nums[i]];
+            prevIdx++;
+        }
     }
-    return prevIdx-1;
+    return prevIdx;
 };

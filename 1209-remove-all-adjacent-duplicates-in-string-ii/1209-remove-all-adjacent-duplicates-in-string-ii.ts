82988193472ca07d7f907b63213e398b81b5048b
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-20 08:48:40 -0500

    Time: 25 ms (29.9%), Space: 67.2 MB (15.46%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.ts
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.ts
@@ -3,13 +3,9 @@ function removeDuplicates(s: string, k: number): string {
 
     for(const char of s) {
         if(stack.length > 0 && stack[stack.length - 1][0] === char) {
-            if(stack[stack.length - 1][1] + 1 === k) {
-                while(stack.length > 0 && stack[stack.length - 1][0] === char) stack.pop();
-            } else {
-                const prevCount = stack[stack.length - 1][1] + 1;
-                stack.push([char, prevCount]);
-            }
+           stack[stack.length - 1][1] +=1;
+           if(stack[stack.length - 1][1] === k) stack.pop();
         } else stack.push([char,1]);
     }
-   return stack.map(([a,b]) => a).join('');
+   return stack.map(([a,b]) => a.repeat(b)).join('');
 };

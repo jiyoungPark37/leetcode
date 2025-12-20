871247521f7d9344f5831ca86c697d118dbc45d4
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-20 16:50:45 -0500

    Time: 0 ms (100%), Space: 56.4 MB (37.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,13 +2,8 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    if(n === 0) return;
-    let idx = 0;
-    for(let i = 0; i < nums1.length; i++) {
-        if(nums1[i] === 0 && idx < n) {
-            nums1[i] = nums2[idx];
-            idx+=1;
-        }
+    for(let i = m; i < m+n; i++) {
+        nums1[i] = nums2[i-m];
     }
     nums1.sort((a,b) => a - b);
 };

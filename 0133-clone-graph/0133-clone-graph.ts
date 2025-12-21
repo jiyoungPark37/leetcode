75ac63743bb7dc9a5ce742fe639aa61098190466
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-21 09:54:45 -0500

    Time: 49 ms (64.4%), Space: 57.4 MB (89.78%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.ts
+++ 0133-clone-graph/0133-clone-graph.ts
@@ -1,30 +1,38 @@
 /**
- * Definition for Node.
- * class Node {
+ * Definition for _Node.
+ * class _Node {
  *     val: number
- *     neighbors: Node[]
- *     constructor(val?: number, neighbors?: Node[]) {
+ *     neighbors: _Node[]
+ * 
+ *     constructor(val?: number, neighbors?: _Node[]) {
  *         this.val = (val===undefined ? 0 : val)
  *         this.neighbors = (neighbors===undefined ? [] : neighbors)
  *     }
  * }
+ * 
  */
-const dfs = (node: Node | null, memo: Map<Node, Node>) => {
-    if(!node) return;
 
-    if(memo.has(node)) return memo.get(node);
 
-    const newNode = new Node(node.val);
-    memo.set(node, newNode);
+function cloneGraph(node: _Node | null): _Node | null {
+	if(!node) return node;
 
-    for(let i = 0; i < node.neighbors.length; i++) {
-        newNode.neighbors.push(dfs(node.neighbors[i], memo))
-    };
-    return newNode;
-};
+    const queue = [];
+    const map = new Map();
+    const cloneNode = new _Node(node.val);
+    map.set(node, cloneNode);
+    queue.push(node);
+    while(queue.length > 0) {
+        const original = queue.shift();
+        const cloneNode = map.get(original);
 
-function cloneGraph(node: Node | null): Node | null {
-    let memo: Map<Node, Node> = new Map();
-
-    return dfs(node, memo);
+        for(const neighbor of original.neighbors) {
+            if(!map.has(neighbor)) {
+                const cloneNeighbor = new _Node(neighbor.val);
+                map.set(neighbor, cloneNeighbor);
+                queue.push(neighbor);
+            }
+            cloneNode.neighbors.push(map.get(neighbor));
+        }
+    }
+    return cloneNode;
 };

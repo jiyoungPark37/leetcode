cb3bda1752bbc26ae5e0134d0c0ace17ba980694
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-21 08:44:36 -0500

    Time: 2 ms (41.8%), Space: 58.8 MB (12.7%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.ts
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -1,20 +1,19 @@
 function generateParenthesis(n: number): string[] {
-    const len = n * 2;
+    const total = n * 2;
     const answer = [];
 
-    const traverse = (cur: string[], left: number, right: number) => {
-        if(cur.length === len) {
+    const dfs = (cur, open, close) => {
+        if(cur.length === total) {
             answer.push(cur.join(''));
             return;
         }
-
-        if(left < n) {
-            traverse([...cur, '('], left + 1, right);
+        if(open < n) {
+            dfs([...cur, '('], open+1, close);
         }
-        if(right < left) {
-            traverse([...cur, ')'], left, right + 1);
+        if(close < open) {
+            dfs([...cur, ')'], open, close+1);
         }
     }
-    traverse(['('], 1, 0);
+    dfs(['('], 1, 0);
     return answer;
 };

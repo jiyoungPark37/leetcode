3e26408033401b813a1c02a828f3a851eeca7eee
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-22 07:36:21 -0500

    Time: 57 ms (71.73%), Space: 65.8 MB (35.96%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -13,15 +13,11 @@
  */
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    if (root === null) return null;
-
-    if (root === p || root === q) return root;
+	if(root === null) return null;
+    if(root === p || root === q) return root;
     
-    const left: TreeNode | null = lowestCommonAncestor(root.left, p, q);
-    const right: TreeNode | null = lowestCommonAncestor(root.right, p, q);
-
-    if (left !== null && right !== null)
-        return root;
-    
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+    if(left !== null && right !== null) return root;
     return left || right;
 };

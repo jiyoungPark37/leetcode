ec3ff0bd92bdbd0fd427f11f60a4b24957053cd9
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 10:55:02 -0500

    Time: 0 ms (100%), Space: 55.7 MB (31.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,11 +1,10 @@
 function removeElement(nums: number[], val: number): number {
     let prevIdx = 0;
-
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] !== val) {
-            [nums[prevIdx], nums[i]] = [nums[i], nums[prevIdx]];
+            nums[prevIdx] = nums[i];
             prevIdx++;
-        } 
+        }
     }
     return prevIdx;
 };

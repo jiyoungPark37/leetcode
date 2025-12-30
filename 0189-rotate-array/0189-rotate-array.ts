401d490734a10b0520ac8ba1f0ea86b28d3d37ce
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 10:58:40 -0500

    Time: 771 ms (17.77%), Space: 67.4 MB (37.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -2,8 +2,9 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    while(k > 0) {
+    let count = k;
+    while(count > 0) {
         nums.unshift(nums.pop());
-        k-=1;
+        count--;
     }
 };

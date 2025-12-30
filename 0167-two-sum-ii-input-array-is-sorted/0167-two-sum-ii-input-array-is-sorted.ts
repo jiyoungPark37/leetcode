ef3a408b66974343b140ac7583fe7f5b0921254a
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 15:57:05 -0500

    Time: 2 ms (56.38%), Space: 57.2 MB (23.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -2,7 +2,6 @@ function twoSum(numbers: number[], target: number): number[] {
     const map = new Map();
     for(let i = 0; i < numbers.length; i++) {
         const diff = target - numbers[i];
-        console.log(diff, 'target')
         if(map.has(diff)) return [map.get(diff), i+1];
         map.set(numbers[i], i+1);
     }

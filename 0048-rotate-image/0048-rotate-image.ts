1d2ab166a5080e1aba2c8b245088abdbb564334a
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 18:21:42 -0500

    Time: 1 ms (24.48%), Space: 56.5 MB (44.14%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.ts
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -3,7 +3,7 @@
  */
 function rotate(matrix: number[][]): void {
     for(let i = 0; i < matrix.length; i++) {
-        for(let j = i; j < matrix[0].length; j++) {
+        for(let j = i+1; j < matrix[0].length; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
         matrix[i].reverse();

f5f6e57a043eadea4346648885f8852654274f63
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 17:02:18 -0500

    Time: 8 ms (47.6%), Space: 59.7 MB (62.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,14 +1,14 @@
 function lengthOfLongestSubstring(s: string): number {
-    let idx = 0;
     const set = new Set();
+    let idx = 0;
     let answer = 0;
     for(let i = 0; i < s.length; i++) {
         while(set.has(s[i])) {
             set.delete(s[idx]);
             idx++;
         }
+        answer = Math.max(answer, i - idx + 1);
         set.add(s[i]);
-        if(answer < set.size) answer = set.size;
     }
     return answer;
 };

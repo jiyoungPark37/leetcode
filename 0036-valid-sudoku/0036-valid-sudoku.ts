a4ab3bb945165c1a03d8a9f71aaf159e884290ea
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 17:39:02 -0500

    Time: 4 ms (76.13%), Space: 57.2 MB (96.19%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.ts
+++ 0036-valid-sudoku/0036-valid-sudoku.ts
@@ -1,15 +1,15 @@
 function isValidSudoku(board: string[][]): boolean {
     const set = new Set();
-    const DOT = '.';
-
     for(let i = 0; i < board.length; i++) {
         for(let j = 0; j < board[0].length; j++) {
-            if(board[i][j] === DOT) continue;
-            const keyI = Math.floor(i / 3);
-            const keyJ = Math.floor(j / 3);
-            const key = `keyI:${keyI}-keyJ:${keyJ}:${board[i][j]}`;
-            if(set.has(key)) return false;
-            set.add(key);
+            if(board[i][j] === '.') continue;
+            const colKey = `i:${i}:${board[i][j]}`;
+            const rowKey = `j:${j}:${board[i][j]}`;
+            const rowColKey = `i:${Math.floor(i/3)}-j:${Math.floor(j/3)}:${board[i][j]}`;
+            if(set.has(colKey) || set.has(rowKey) || set.has(rowColKey)) return false;
+            set.add(colKey);
+            set.add(rowKey);
+            set.add(rowColKey);
         }
     }
     return true;

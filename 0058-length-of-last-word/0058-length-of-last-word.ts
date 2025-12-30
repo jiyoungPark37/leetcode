e8b348311202a8343f12e288203aed9646b20532
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 15:05:53 -0500

    Time: 0 ms (100%), Space: 54.9 MB (70.58%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,4 +1,4 @@
 function lengthOfLastWord(s: string): number {
-    const arr = s.trim().split(' ');
-    return arr[arr.length - 1].length;
+    const strs = s.trim().split(/\s+/);
+    return strs[strs.length - 1].length;
 };

20a7756421dd4eb3d88884787c42f6d58729df46
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 11:18:28 -0500

    Time: 0 ms (100%), Space: 60.2 MB (27.5%) - LeetHub

--- 0055-jump-game/0055-jump-game.ts
+++ 0055-jump-game/0055-jump-game.ts
@@ -1,8 +1,9 @@
 function canJump(nums: number[]): boolean {
-    let curMax = 0;
-    for(let i = 0; i < nums.length; i++) {
-        curMax = Math.max(curMax - 1, nums[i]);
-        if(curMax <= 0 && i < nums.length - 1) return false;
+    if(nums.length <= 1) return true;
+    let max = nums[0];
+    for(let i = 0; i < nums.length - 1; i++) {
+        max = Math.max(max-1, nums[i]);
+        if(max === 0) return false;
     }
     return true;
 };

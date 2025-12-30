6805ec82635d0321e9cfce56c5eb25af17bf76ba
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 14:59:29 -0500

    Time: 7 ms (36.4%), Space: 62 MB (43.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.ts
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -1,27 +1,32 @@
-const ROMAN_SYMBOL = {
-    'I': 1,
+const ROMAN_DIGIT = {
+    'I':1,
+    'IV':4,
     'V':5,
+    'IX':9,
     'X':10,
+    'XL':40,
     'L':50,
+    'XC':90,
     'C':100,
+    'CD':400,
     'D':500,
+    'CM': 900,
     'M':1000,
-}
-const REPLACE = {
-    'I': ['V', 'X'],
-    'X': ['L', 'C'],
-    'C': ['D','M'],
 }
 function romanToInt(s: string): number {
     let answer = 0;
-
     for(let i = 0; i < s.length; i++) {
-        const cur = ROMAN_SYMBOL[s[i]];
-        if(['I', 'X', 'C'].includes(s[i]) && REPLACE[s[i]].includes(s[i+1])) {
-            const next = ROMAN_SYMBOL[s[i+1]];
-            answer += (next-cur);
-            i+=1;
-        } else answer+= cur;
+        const cur = s[i];
+        const next = s[i+1];
+        if(ROMAN_DIGIT[cur]) {
+            const key = `${cur}${next}`
+            if(ROMAN_DIGIT[key]) {
+                answer+=ROMAN_DIGIT[key];
+                i++;
+            } else {
+                answer+=ROMAN_DIGIT[cur];
+            }
+        }
     }
     return answer;
 };

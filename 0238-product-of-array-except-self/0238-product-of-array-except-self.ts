2b9321118b489da747598bab32de6608ff16745a
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 13:06:54 -0500

    Time: 5 ms (79.78%), Space: 68.9 MB (88.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,10 +1,11 @@
 function productExceptSelf(nums: number[]): number[] {
-    let prev = nums[0];
     const arr = Array(nums.length).fill(1);
+    let prev = nums[0];
     for(let i = 1; i < nums.length; i++) {
         arr[i] = prev;
         prev*=nums[i];
     }
+
     prev = nums[nums.length - 1];
     for(let i = nums.length - 2; i >= 0; i--) {
         arr[i] *= prev;

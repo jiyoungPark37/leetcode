f223cda345b5121a28b4e769895b9d0db4bdbb3c
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-31 11:54:18 -0500

    Time: 13 ms (68.18%), Space: 63.5 MB (57.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.ts
+++ 0207-course-schedule/0207-course-schedule.ts
@@ -1,21 +1,18 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-    const relations = Array.from({length: numCourses}, () => []);
+    const relations = Array.from({length:numCourses}, () => []);
     const indegree = Array(numCourses).fill(0);
+    const queue = [];
 
     for(const [next, prev] of prerequisites) {
-        indegree[next]+=1;
         relations[prev].push(next);
+        indegree[next]++;
     }
-    const queue = [];
     indegree.forEach((ele, idx) => ele === 0 && queue.push(idx));
-
-    const answer = [];
     while(queue.length > 0) {
-        const taken = queue.shift();
-        answer.push(taken);
-        for(const course of relations[taken]){
+        const shift = queue.shift();
+        for(const course of relations[shift]) {
             if(--indegree[course] === 0) queue.push(course);
         }
     }
-    return answer.length === numCourses;
-}; 
+    return indegree.every(ele => ele === 0);
+};

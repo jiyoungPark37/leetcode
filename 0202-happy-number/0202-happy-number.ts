3956bcf7c0f495e7922af105810864a40448756d
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-30 20:12:01 -0500

    Time: 2 ms (54.51%), Space: 57.8 MB (43.75%) - LeetHub

--- 0202-happy-number/0202-happy-number.ts
+++ 0202-happy-number/0202-happy-number.ts
@@ -1,12 +1,17 @@
 function isHappy(n: number): boolean {
-    const set = new Set();
-    let trackingNum = n;
-
-    while(true) {
-        trackingNum = String(trackingNum).split('').reduce((acc, cur) => Number(cur) * Number(cur) + acc, 0);
-        if(trackingNum === 1) return true;
-        if(set.has(trackingNum)) return false;
-        set.add(trackingNum);
+    let cur = n;
+    const map = new Map();
+    map.set(1, true);
+    map.set(2, false);
+    while(cur > 1) {
+        const str = String(cur);
+        let sum = 0;
+        for(let i = 0; i < str.length; i++) {
+            sum+=Number(str[i]) * Number(str[i]);
+        }
+        if(map.has(sum)) return map.get(sum);
+        map.set(sum, false);
+        cur = sum;
     }
-    return false;
+    return true;
 };

f0a5fa4d252d8b42c75ac39f0308368d34a59770
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-31 12:13:38 -0500

    Time: 246 ms (92.94%), Space: 56.7 MB (64.37%) - LeetHub

--- 0079-word-search/0079-word-search.ts
+++ 0079-word-search/0079-word-search.ts
@@ -1,21 +1,21 @@
 function exist(board: string[][], word: string): boolean {
-    const dir = [[0,1],[1,0],[0,-1],[-1,0]];
+    let answer = false;
+    const hasVisited = '999';
+    const dist = [[0,1],[1,0],[0,-1],[-1,0]];
     const n = board.length;
     const m = board[0].length;
-    let answer = false;
-
-    const dfs = (x:number, y:number, idx:number) => {
+    const dfs = (x,y, idx) => {
         if(answer) return;
         if(idx >= word.length) {
             answer = true;
             return;
         }
         for(let i = 0; i < 4; i++) {
-            const nx = x + dir[i][0];
-            const ny = y + dir[i][1];
+            const nx = x + dist[i][0];
+            const ny = y + dist[i][1];
             if(nx >= 0 && ny >= 0 && nx < n && ny < m && board[nx][ny] === word[idx]) {
-                board[nx][ny] = 'O';
-                dfs(nx,ny, idx+1);
+                board[nx][ny] = hasVisited;
+                dfs(nx, ny, idx+1);
                 board[nx][ny] = word[idx];
             }
         }
@@ -23,7 +23,7 @@ function exist(board: string[][], word: string): boolean {
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
             if(board[i][j] === word[0]) {
-                board[i][j] = '0';
+                board[i][j] = hasVisited;
                 dfs(i, j, 1);
                 board[i][j] = word[0];
             }

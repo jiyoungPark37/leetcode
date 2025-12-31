c95016c8d70a5ecfb97c7eee4b37f7982c1e1538
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-31 12:23:30 -0500

    Time: 0 ms (100%), Space: 55.4 MB (49.52%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,27 +1,28 @@
-const DIGITS = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-}
 function letterCombinations(digits: string): string[] {
+    const DIGITS = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz'
+    };
     const answer = [];
-    const dfs = (idx, str) => {
-        if(idx >= digits.length) {
-            answer.push(str.join(''));
+    const dfs = (cur, idx) => {
+        if(cur.length === digits.length) {
+            answer.push(cur.join(''));
             return;
         }
-        const cur = DIGITS[digits[idx]];
-        for(const char of cur) {
-            str.push(char);
-            dfs(idx+1, str);
-            str.pop();
+        if(idx >= digits.length) return;
+        const nextLetters = DIGITS[digits[idx]];
+        for(let i = 0; i < nextLetters.length; i++) {
+            cur.push(nextLetters[i]);
+            dfs(cur, idx+1);
+            cur.pop();
         }
     }
-    dfs(0,[]);
+    dfs([], 0);
     return answer;
 };

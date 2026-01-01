4a8718727399d3b295b6e36f953c7520e05fbb4e
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-31 23:43:55 -0500

    Time: 24 ms (53.2%), Space: 58.3 MB (62.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -1,18 +1,17 @@
 function longestPalindrome(s: string): string {
     let answer = '';
-
-    for(let i = 0; i < s.length; i++) {
+    const n = s.length;
+    for(let i = 0; i < n; i++) {
         let left = i;
-        let right = i + 1;
-        while(left >= 0 && right < s.length && s[left] === s[right]) {
+        let right = i+1;
+        while(left >= 0 && right < n && s[left] === s[right]) {
             if(answer.length < right - left + 1) answer = s.substring(left, right + 1);
             left--;
             right++;
         }
-
         left = i;
-        right = i;
-        while(left >= 0 && right < s.length && s[left] === s[right]) {
+        right=i;
+        while(left >= 0 && right < n && s[left] === s[right]) {
             if(answer.length < right - left + 1) answer = s.substring(left, right + 1);
             left--;
             right++;

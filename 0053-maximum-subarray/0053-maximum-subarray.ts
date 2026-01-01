805851125f8d4d1eb5c4bc4cdbde91056549f9ae
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2025-12-31 23:19:39 -0500

    Time: 3 ms (40.61%), Space: 66.9 MB (9.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -1,10 +1,7 @@
 function maxSubArray(nums: number[]): number {
-    let max = Math.max(...nums);
     const arr = nums.slice();
-
-    for(let i = 0; i < arr.length; i++) {
-        const curMax = i > 0 ? arr[i-1] : 0;
-        arr[i] = Math.max(arr[i], arr[i] + curMax);
+    for(let i =1; i < nums.length; i++) {
+        arr[i] = Math.max(arr[i], arr[i-1]+arr[i]);
     }
     return Math.max(...arr);
 };

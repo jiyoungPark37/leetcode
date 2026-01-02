4ff5261395d984d31db47f051bfd8c3a114ee14c
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2026-01-01 19:59:24 -0500

    Time: 3 ms (58.41%), Space: 59.5 MB (71.79%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
@@ -1,17 +1,15 @@
 function minSubArrayLen(target: number, nums: number[]): number {
-    const totalSum = nums.reduce((sum, acc) => acc+sum, 0);
-    if(totalSum < target) return 0;
-    let answer = nums.length;
-    let idx = 0;
+    let size = Infinity;
     let sum = 0;
+    let left = 0;
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] >= target) return 1;
         sum+=nums[i];
-        while(idx < i && target <= sum) {
-            if(target <= sum && answer > i - idx + 1) answer = i - idx + 1;
-            sum-=nums[idx];
-            idx++;
+        while(sum >= target && left < i) {
+            size = Math.min(size, i - left + 1);
+            sum-=nums[left];
+            left++;
         }
     }
-    return answer;
+    return size === Infinity ? 0 : size;
 };

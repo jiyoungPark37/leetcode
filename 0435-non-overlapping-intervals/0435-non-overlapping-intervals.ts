3cee4be892672f6090edaa66bb78d639eca51147
Jiyoung Park <78076790+jiyoungPark37@users.noreply.github.com>
2026-01-16 12:51:08 -0500

    Time: 79 ms (48.1%), Space: 85.3 MB (92.39%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.ts
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.ts
@@ -1,16 +1,11 @@
 function eraseOverlapIntervals(intervals: number[][]): number {
     intervals.sort((a,b) => a[1] - b[1]);
-    let tmp = intervals[0][1];
-    let answer = 0;
+    let count = 0;
+    let prevEnd = intervals[0][1];
     for(let i = 1; i < intervals.length; i++) {
-        let start = intervals[i][0];
-        let end = intervals[i][1];
-
-        if(start >= tmp) {
-            tmp = end;
-        } else {
-            answer++;
-        }
-    };
-    return answer;
+        const [start,end] = intervals[i];
+        if(prevEnd <= start) prevEnd = end;
+        else count++;
+    }
+    return count;
 };
